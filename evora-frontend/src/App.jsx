84d0206eb847bf6c07ec0bd301ebbb9659377803
Sahan Sanjaya <as20240912@sci.sjp.ac.lk>
2026-07-28 09:45:25 +0530

    Updated driver page and UI fixes

--- evora-frontend/src/App.jsx
+++ evora-frontend/src/App.jsx
@@ -1,16 +1,27 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import LandingPage from './pages/LandingPage'
-import BookCharger from './pages/BookCharger'
+import { Routes, Route, Navigate } from 'react-router-dom'
+import ChooseAccountType from './pages/ChooseAccountType.jsx'
+import Login from './pages/Login.jsx'
+import ForgotPassword from './pages/ForgotPassword.jsx'
+import CompleteProfile from './pages/CompleteProfile.jsx'
+import CreateDriverAccount from './pages/CreateDriverAccount.jsx'
+import CreateHostAccount from './pages/CreateHostAccount.jsx'
+import OtpVerification from './pages/OtpVerification.jsx'
+import AccountCreated from './pages/AccountCreated.jsx'
+import Dashboard from './pages/Dashboard.jsx'
 
-function App() {
+export default function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<LandingPage />} />
-        <Route path="/book-charger" element={<BookCharger />} />
-      </Routes>
-    </BrowserRouter>
+    <Routes>
+      <Route path="/" element={<ChooseAccountType />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/forgot-password" element={<ForgotPassword />} />
+      <Route path="/complete-profile" element={<CompleteProfile />} />
+      <Route path="/create-account/driver" element={<CreateDriverAccount />} />
+      <Route path="/create-account/host" element={<CreateHostAccount />} />
+      <Route path="/otp-verification" element={<OtpVerification />} />
+      <Route path="/account-created" element={<AccountCreated />} />
+      <Route path="/dashboard" element={<Dashboard />} />
+      <Route path="*" element={<Navigate to="/" replace />} />
+    </Routes>
   )
 }
-
-export default App
--- evora-frontend/src/main.jsx
+++ evora-frontend/src/main.jsx
@@ -1,10 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
 import App from './App.jsx'
+import { AccountTypeProvider } from './context/AccountTypeContext.jsx'
+import './index.css'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <AccountTypeProvider>
+        <App />
+      </AccountTypeProvider>
+    </BrowserRouter>
+  </React.StrictMode>,
 )
