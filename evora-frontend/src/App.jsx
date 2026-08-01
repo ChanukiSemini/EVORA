589b1249caa79acab42da6869044902e795cfa39
styloq99-arch <chanukisemini16@gmail.com>
2026-08-01 10:39:40 +0530

    feat: implement booking confirmation modal and add routing configuration

--- evora-frontend/src/App.jsx
+++ evora-frontend/src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import LandingPage from './pages/LandingPage'
 import BookCharger from './pages/BookCharger'
+import BookingConfirmed from './pages/BookingConfirmed'
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Routes>
         <Route path="/" element={<LandingPage />} />
         <Route path="/book-charger" element={<BookCharger />} />
+        <Route path="/booking-confirmed" element={<BookingConfirmed />} />
       </Routes>
     </BrowserRouter>
   )
