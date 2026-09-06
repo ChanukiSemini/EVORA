dfba76d13a531bcbf0851e7db7c44ffaca7d06e0
ChanukiSemini <chanukisemini16@gmail.com>
2026-09-06 21:57:02 +0530

    fix(router): resolve parse error by removing duplicate imports

--- evora-frontend/src/App.jsx
+++ evora-frontend/src/App.jsx
@@ -6,8 +6,6 @@ import AccountType from './pages/AccountType'
 import FindStation from './pages/FindStation'
 import StationDetails from './pages/StationDetails'
 import BookCharger from './pages/BookCharger'
-import FindStation from './pages/FindStation'
-import StationDetails from './pages/StationDetails'
 import BookingConfirmed from './pages/BookingConfirmed'
 import MyReservations from './pages/MyReservations'
 import BookingDetails from './pages/BookingDetails'
@@ -58,7 +56,6 @@ function App() {
         <Route path="/find-station" element={<FindStation />} />
         <Route path="/station/:id" element={<StationDetails />} />
         <Route path="/book-charger" element={<BookCharger />} />
-        <Route path="/station/:id" element={<StationDetails />} />
         <Route path="/booking-confirmed" element={<BookingConfirmed />} />
         <Route path="/bookings" element={<MyReservations />} />
         <Route path="/booking-details" element={<BookingDetails />} />
