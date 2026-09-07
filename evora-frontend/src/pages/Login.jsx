d831dac7622af5f4e99189f79ccafc5dd2a5bc25
ChanukiSemini <chanukisemini16@gmail.com>
2026-09-07 20:33:25 +0530

    refactor(auth): remove Google and Facebook social login buttons from login page

--- evora-frontend/src/pages/Login.jsx
+++ evora-frontend/src/pages/Login.jsx
@@ -64,14 +64,6 @@ export default function Login() {
     }
   };
 
-  const handleSocialLogin = (provider) => {
-    setIsLoading(true);
-    setTimeout(() => {
-      setIsLoading(false);
-      navigate('/book-charger');
-    }, 400);
-  };
-
   return (
     <div className="login-page">
       {/* Background layer for overlay/wallpaper */}
@@ -214,55 +206,6 @@ export default function Login() {
                 {isLoading ? 'Logging in...' : 'Login'}
               </button>
             </form>
-
-            {/* Divider */}
-            <div className="login-divider">
-              <span className="divider-line" />
-              <span className="divider-text">or continue with</span>
-              <span className="divider-line" />
-            </div>
-
-            {/* Social Logins */}
-            <div className="login-social-group">
-              {/* Google */}
-              <button
-                type="button"
-                className="btn-social-login"
-                onClick={() => handleSocialLogin('Google')}
-              >
-                <svg className="social-icon" viewBox="0 0 24 24" width="20" height="20">
-                  <path
-                    fill="#EA4335"
-                    d="M12 5c1.6 0 3 .6 4.1 1.7l3.1-3.1C17.3 1.8 14.8 1 12 1 7.5 1 3.7 3.6 1.9 7.3l3.7 2.9C6.5 7.4 9 5 12 5z"
-                  />
-                  <path
-                    fill="#4285F4"
-                    d="M23.5 12.3c0-.8-.1-1.6-.2-2.3H12v4.5h6.5c-.3 1.5-1.1 2.8-2.4 3.7l3.7 2.9c2.2-2 3.7-5 3.7-8.8z"
-                  />
-                  <path
-                    fill="#FBBC05"
-                    d="M5.6 14.8c-.2-.7-.4-1.5-.4-2.8s.2-2.1.4-2.8L1.9 6.3C.7 8.7 0 10.3 0 12s.7 3.3 1.9 5.7l3.7-2.9z"
-                  />
-                  <path
-                    fill="#34A853"
-                    d="M12 23c3.2 0 6-1.1 8-3l-3.7-2.9c-1.1.7-2.5 1.2-4.3 1.2-3 0-5.5-2.4-6.4-5.2L1.9 16C3.7 19.7 7.5 23 12 23z"
-                  />
-                </svg>
-                <span>Login with Google</span>
-              </button>
-
-              {/* Facebook */}
-              <button
-                type="button"
-                className="btn-social-login"
-                onClick={() => handleSocialLogin('Facebook')}
-              >
-                <svg className="social-icon" viewBox="0 0 24 24" width="20" height="20" fill="#1877F2">
-                  <path d="M24 12.073c0-6.627-5.373-12-12-12s-12 5.373-12 12c0 5.99 4.388 10.954 10.125 11.854v-8.385H7.078v-3.47h3.047V9.43c0-3.007 1.792-4.669 4.533-4.669 1.312 0 2.686.235 2.686.235v2.953H15.83c-1.491 0-1.956.925-1.956 1.874v2.25h3.328l-.532 3.47h-2.796v8.385C19.612 23.027 24 18.062 24 12.073z" />
-                </svg>
-                <span>Login with Facebook</span>
-              </button>
-            </div>
 
             {/* Footer / Create Account Link */}
             <div className="login-card-footer">
